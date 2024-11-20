a1d26fa1f0318b3b2315bd2b38de8ed502913ca1
Facundo Cubells <facundocubells@gmail.com>
2024-11-20 10:22:20 -0300

    Actualizacion

--- server.js
+++ server.js
@@ -19,7 +19,12 @@ app.use(express.json());
 
 // Conexión a MongoDB
 mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 30000, // Tiempo de espera más alto
+  socketTimeoutMS: 45000,         // Tiempo de espera del socket
+})
   .then(() => console.log('MongoDB conectado'))
   .catch((err) => console.error('Error al conectar a MongoDB:', err));
 
